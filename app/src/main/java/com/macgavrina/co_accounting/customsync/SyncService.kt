da418023415d628467da0d7977126a25cf7fbe82
Irina Gavrina <ivgavrina@gmail.com>
2019-07-19 16:02:46 +0300

    Delete unused permissions and classes

--- app/src/main/java/com/macgavrina/co_accounting/customsync/SyncService.kt
+++ app/src/main/java/com/macgavrina/co_accounting/customsync/SyncService.kt
@@ -1,50 +1,50 @@
-package com.macgavrina.co_accounting.customsync
-
-import com.macgavrina.co_accounting.MainApplication
-import com.macgavrina.co_accounting.logging.Log
-import com.macgavrina.co_accounting.model.DefaultServiceResponse
-import com.macgavrina.co_accounting.model.RecoverPassResponse
-import com.macgavrina.co_accounting.rxjava.Events
-import com.macgavrina.co_accounting.services.AuthService
-import com.macgavrina.co_accounting.services.ContactsService
-import com.macgavrina.co_accounting.sync.SyncAdapter
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.observers.DisposableSingleObserver
-import io.reactivex.schedulers.Schedulers
-
-class SyncService() {
-
-//    companion object {
+//package com.macgavrina.co_accounting.customsync
 //
-//        const val SYNC_UPLOAD = "upload"
-//        const val SYNC_DOWNLOAD = "download"
+//import com.macgavrina.co_accounting.MainApplication
+//import com.macgavrina.co_accounting.logging.Log
+//import com.macgavrina.co_accounting.model.DefaultServiceResponse
+//import com.macgavrina.co_accounting.model.RecoverPassResponse
+//import com.macgavrina.co_accounting.rxjava.Events
+//import com.macgavrina.co_accounting.services.AuthService
+//import com.macgavrina.co_accounting.services.ContactsService
+//import com.macgavrina.co_accounting.sync.SyncAdapter
+//import io.reactivex.android.schedulers.AndroidSchedulers
+//import io.reactivex.observers.DisposableSingleObserver
+//import io.reactivex.schedulers.Schedulers
 //
-//        fun syncData(syncUpload: Boolean, syncDownload: Boolean, userToken: String) {
+//class SyncService() {
 //
-//            if (syncUpload) {
-//                Log.d("start syncUpload")
-//                val contactsService: ContactsService = ContactsService.create()
-//
-//                contactsService.addContact(userToken, "testEmail", "testAlias")
-//                        .subscribeOn(Schedulers.io())
-//                        .observeOn(AndroidSchedulers.mainThread())
-//                        .subscribeWith(object : DisposableSingleObserver<DefaultServiceResponse>() {
-//                            override fun onSuccess(t: DefaultServiceResponse) {
-//                                Log.d("test contact is added")
-//                            }
-//
-//                            override fun onError(e: Throwable) {
-//                                Log.d("adding test contact failed with error = ${e.message}")
-//
-//                            }
-//                        })
-//
-//            }
-//
-//            if (syncDownload) {
-//                Log.d("syncDownload")
-//            }
-//
-//        }
-//    }
-}
+////    companion object {
+////
+////        const val SYNC_UPLOAD = "upload"
+////        const val SYNC_DOWNLOAD = "download"
+////
+////        fun syncData(syncUpload: Boolean, syncDownload: Boolean, userToken: String) {
+////
+////            if (syncUpload) {
+////                Log.d("start syncUpload")
+////                val contactsService: ContactsService = ContactsService.create()
+////
+////                contactsService.addContact(userToken, "testEmail", "testAlias")
+////                        .subscribeOn(Schedulers.io())
+////                        .observeOn(AndroidSchedulers.mainThread())
+////                        .subscribeWith(object : DisposableSingleObserver<DefaultServiceResponse>() {
+////                            override fun onSuccess(t: DefaultServiceResponse) {
+////                                Log.d("test contact is added")
+////                            }
+////
+////                            override fun onError(e: Throwable) {
+////                                Log.d("adding test contact failed with error = ${e.message}")
+////
+////                            }
+////                        })
+////
+////            }
+////
+////            if (syncDownload) {
+////                Log.d("syncDownload")
+////            }
+////
+////        }
+////    }
+//}
--- app/src/main/java/com/macgavrina/co_accounting/sync/AuthenticatorService.kt
+++ app/src/main/java/com/macgavrina/co_accounting/sync/AuthenticatorService.kt
@@ -1,24 +1,24 @@
-package com.macgavrina.co_accounting.sync
-
-import android.app.Service
-import android.content.Intent
-import android.os.IBinder
-
-/**
- * A bound Service that instantiates the authenticator
- * when started.
- */
-
-class AuthenticatorService: Service() {
-
-    lateinit var mAuthenticator:Authenticator
-
-    override fun onCreate() {
-        super.onCreate()
-        mAuthenticator = Authenticator(this)
-    }
-    override fun onBind(p0: Intent?): IBinder? {
-        return mAuthenticator.iBinder
-    }
-
-}
+//package com.macgavrina.co_accounting.sync
+//
+//import android.app.Service
+//import android.content.Intent
+//import android.os.IBinder
+//
+///**
+// * A bound Service that instantiates the authenticator
+// * when started.
+// */
+//
+//class AuthenticatorService: Service() {
+//
+//    lateinit var mAuthenticator:Authenticator
+//
+//    override fun onCreate() {
+//        super.onCreate()
+//        mAuthenticator = Authenticator(this)
+//    }
+//    override fun onBind(p0: Intent?): IBinder? {
+//        return mAuthenticator.iBinder
+//    }
+//
+//}
